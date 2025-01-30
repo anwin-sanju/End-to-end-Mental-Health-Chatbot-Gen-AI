7102325d1a6f7481e6e4c95f6502eb6771f03fd1
anwin-sanju <dev.anwinsanju@gmail.com>
2025-01-30 10:21:26 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mental Health AI Chatbot Project',
+    version= '0.0.0',
+    author= 'Anwin Sanju',
+    author_email= 'anwinsanju17@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
